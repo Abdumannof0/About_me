4122a6a0d60e858d6296b7330c321b7510337aae
Abduvaliev <140178160+Abdumannof0@users.noreply.github.com>
2023-12-09 17:02:41 +0300

    #fix #About_me #tugadi

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-// const TOKEN = "",
-// const CHAT_ID = "",
+const form = document.querySelector('#form')
 
-   const tg =  document.getElementById = ('tg').addEventListener('submit', function (e) {
-    e,preventDefult()
-    console.log('!');
-    })
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let text1 = document.getElementById('text1').value
+    let text2 = document.getElementById('text2').value
+    let text3 = document.getElementById('text3').value
+
+    let my_text = `Resalt is:%0A - Text1: ${text1} %0A - Text2: ${text2} %0A - Text3: ${text3}`
+
+    let token = "6878364378:AAHc6KogYCEwwpEoyQrfvFhVERHIe5D0uW8"
+
+    let chat_id = -4019133542
+
+    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+    let api = new XMLHttpRequest()
+    api.open("GET", url, true)
+    api.send()
+
+    console.log("Message");
+})
